02b89c7e1875c1dab5a82012d2a25b14f5e250f7
Joe LeBlanc <contact@jlleblanc.com>
2011-11-05 23:05:36 -0500

    Added a form to the feed view to test the iTunes Category feature.

--- administrator/components/com_podcast/views/feed/tmpl/default.php
+++ administrator/components/com_podcast/views/feed/tmpl/default.php
@@ -1,1 +1,19 @@
 <?php defined( '_JEXEC' ) or die; ?>
+<form action="index.php?option=com_podcast&amp;feed_id=<?php echo $this->item->feed_id ?>"
+	method="post" name="adminForm" class="form-validate">
+	<div class="width-60 fltlft">
+		<fieldset class="adminform">
+			<legend>Feed</legend>
+			<ul class="adminformlist">
+				<?php foreach ($this->form->getFieldset() as $field): ?>
+					<li><?php echo $field->label; ?>
+					<?php echo $field->input; ?></li>
+				<?php endforeach ?>
+			</ul>
+
+		</fieldset>
+	</div>
+
+	<input type="hidden" name="task" value="" />
+	<?php echo JHtml::_('form.token'); ?>
+</form>
--- administrator/components/com_podcast/views/feed/view.html.php
+++ administrator/components/com_podcast/views/feed/view.html.php
@@ -5,8 +5,13 @@ jimport( 'joomla.application.component.view');
 
 class PodcastViewFeed extends JView
 {
+	protected $form;
+	protected $item;
+
 	public function display($tpl = null)
 	{
+		$this->form = $this->get('Form');
+		$this->item = $this->get('Item');
 
 		parent::display($tpl);
 	}
