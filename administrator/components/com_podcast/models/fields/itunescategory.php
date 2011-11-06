fdc382dce54ff3ccd1303fee383334a0f24334d4
Joe LeBlanc <contact@jlleblanc.com>
2011-11-05 23:05:13 -0500

    Added a field type for iTunes Category.

---
+++ administrator/components/com_podcast/models/fields/itunescategory.php
@@ -0,0 +1,97 @@
+<?php
+defined( '_JEXEC' ) or die;
+
+jimport('joomla.html.html');
+jimport('joomla.form.formfield');
+jimport('joomla.form.helper');
+JFormHelper::loadFieldClass('list');
+
+class JFormFieldItunescategory extends JFormFieldList
+{
+	protected $type = 'Itunescategory';
+	protected $categories = array(
+		'Arts',
+		'Arts > Design',
+		'Arts > Fashion & Beauty',
+		'Arts > Food',
+		'Arts > Literature',
+		'Arts > Performing Arts',
+		'Arts > Visual Arts',
+		'Business',
+		'Business > Business News',
+		'Business > Careers',
+		'Business > Investing',
+		'Business > Management & Marketing',
+		'Business > Shopping',
+		'Comedy',
+		'Education',
+		'Education > Education Technology',
+		'Education > Higher Education',
+		'Education > K-12',
+		'Education > Language Courses',
+		'Education > Training',
+		'Games & Hobbies',
+		'Games & Hobbies > Automotive',
+		'Games & Hobbies > Aviation',
+		'Games & Hobbies > Hobbies',
+		'Games & Hobbies > Other Games',
+		'Games & Hobbies > Video Games',
+		'Government & Organizations',
+		'Government & Organizations > Local',
+		'Government & Organizations > National',
+		'Government & Organizations > Non-Profit',
+		'Government & Organizations > Regional',
+		'Health',
+		'Health > Alternative Health',
+		'Health > Fitness & Nutrition',
+		'Health > Self-Help',
+		'Health > Sexuality',
+		'Kids & Family',
+		'Music',
+		'News & Politics',
+		'Religion & Spirituality',
+		'Religion & Spirituality > Buddhism',
+		'Religion & Spirituality > Christianity',
+		'Religion & Spirituality > Hinduism',
+		'Religion & Spirituality > Islam',
+		'Religion & Spirituality > Judaism',
+		'Religion & Spirituality > Other',
+		'Religion & Spirituality > Spirituality',
+		'Science & Medicine',
+		'Science & Medicine > Medicine',
+		'Science & Medicine > Natural Sciences',
+		'Science & Medicine > Social Sciences',
+		'Society & Culture',
+		'Society & Culture > History',
+		'Society & Culture > Personal Journals',
+		'Society & Culture > Philosophy',
+		'Society & Culture > Places & Travel',
+		'Sports & Recreation',
+		'Sports & Recreation > Amateur',
+		'Sports & Recreation > College & High School',
+		'Sports & Recreation > Outdoor',
+		'Sports & Recreation > Professional',
+		'Technology',
+		'Technology > Gadgets',
+		'Technology > Tech News',
+		'Technology > Podcasting',
+		'Technology > Software How-To',
+		'TV & Film'
+	);
+
+	public function getOptions()
+	{
+		$object_categories = array();
+
+		foreach ($this->categories as $category) {
+			$object = new StdClass;
+			$object->text = $category;
+			$object->value = $category;
+			$object_categories[] = $object;
+		}
+
+		array_unshift($object_categories, JHtml::_('select.option', '', '- Select Category -'));
+
+		return $object_categories;
+	}
+}
