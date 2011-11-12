2611e4067cd90e17b0a0962430ec980642d32674
Joe LeBlanc <contact@jlleblanc.com>
2011-11-12 12:25:29 -0600

    Got toolbars in place.

--- administrator/components/com_podcast/views/feeds/view.html.php
+++ administrator/components/com_podcast/views/feeds/view.html.php
@@ -7,7 +7,34 @@ class PodcastViewFeeds extends JView
 {
 	public function display($tpl = null)
 	{
+		$this->addToolbar();
 
 		parent::display($tpl);
+	}
+
+	public function addToolbar()
+	{
+		JToolBarHelper::title(JText::_('COM_PODCAST_PODCAST_FEEDS_MANAGER'));
+
+		JToolBarHelper::makeDefault('feeds.setDefault', 'COM_PODCAST_SET_DEFAULT_FEED');
+
+		JToolBarHelper::divider();
+
+		JToolBarHelper::publish('feeds.publish');
+		JToolBarHelper::unpublish('feeds.unpublish');
+
+		JToolBarHelper::divider();
+
+		JToolBarHelper::deleteList('', 'feeds.delete');
+
+		JToolBarHelper::divider();
+
+		JToolBarHelper::editList('feed.edit');
+		JToolBarHelper::addNew('feed.add');
+
+		JToolBarHelper::divider();
+
+		JToolBarHelper::preferences('com_podcast');
+		JToolBarHelper::help('replace with custom button');
 	}
 }
--- administrator/components/com_podcast/views/mediaitems/view.html.php
+++ administrator/components/com_podcast/views/mediaitems/view.html.php
@@ -7,7 +7,29 @@ class PodcastViewMediaitems extends JView
 {
 	public function display($tpl = null)
 	{
+		$this->addToolbar();
 
 		parent::display($tpl);
+	}
+
+	public function addToolbar()
+	{
+		JToolBarHelper::title(JText::_('COM_PODCAST_FEED_ITEMS_MANAGER'));
+
+		JToolBarHelper::publish('mediaitems.publish');
+		JToolBarHelper::unpublish('mediaitems.unpublish');
+
+		JToolBarHelper::divider();
+
+		JToolBarHelper::deleteList('', 'mediaitems.delete');
+
+		JToolBarHelper::divider();
+
+		JToolBarHelper::editList('mediaitem.edit');
+		JToolBarHelper::addNew('mediaitem.add');
+
+		JToolBarHelper::divider();
+
+		JToolBarHelper::help('replace with custom button');
 	}
 }
