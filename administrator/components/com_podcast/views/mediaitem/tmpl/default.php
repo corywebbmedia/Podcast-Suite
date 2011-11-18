87eb5420f2de6bdb5eaa624a75928f1d73656c22
Joe LeBlanc <contact@jlleblanc.com>
2011-11-18 16:55:06 -0600

    Finished the basic media item screen, still need to add some pieces for the file selection, etc...

--- administrator/components/com_podcast/views/mediaitem/tmpl/default.php
+++ administrator/components/com_podcast/views/mediaitem/tmpl/default.php
@@ -1,1 +1,60 @@
 <?php defined( '_JEXEC' ) or die; ?>
+
+<form action="index.php?option=com_podcast&amp;media_id=<?php echo $this->item->media_id ?>"
+	method="post" name="adminForm" class="form-validate">
+
+	<div class="width-60 fltlft">
+		<fieldset class="adminform">
+			<legend><?php echo JText::_('COM_PODCAST_MEDIA_ITEM_BASIC'); ?></legend>
+			<ul class="adminformlist">
+				<?php foreach ($this->form->getFieldset('top-col-1') as $field): ?>
+					<li><?php echo $field->label; ?>
+					<?php echo $field->input; ?></li>
+				<?php endforeach ?>
+			</ul>
+
+			<ul class="adminformlist">
+				<?php foreach ($this->form->getFieldset('top-col-2') as $field): ?>
+					<li><?php echo $field->label; ?>
+					<?php echo $field->input; ?></li>
+				<?php endforeach ?>
+			</ul>
+
+		</fieldset>
+
+		<fieldset class="adminform">
+
+			<ul class="adminformlist">
+				<?php foreach ($this->form->getFieldset('bottom') as $field): ?>
+					<li><?php echo $field->label; ?>
+					<?php echo $field->input; ?></li>
+				<?php endforeach ?>
+			</ul>
+
+
+		</fieldset>
+	</div>
+
+	<div class="width-40 fltrt">
+		<fieldset class="adminform">
+			<legend><?php echo JText::_('COM_PODCAST_MEDIA_ITEM_EXTENDED'); ?></legend>
+
+			<ul class="adminformlist">
+				<?php foreach ($this->form->getFieldset('right') as $field): ?>
+					<li><?php echo $field->label; ?>
+					<?php echo $field->input; ?></li>
+				<?php endforeach ?>
+			</ul>
+
+
+		</fieldset>
+
+	</div>
+
+	<input type="hidden" name="task" value="" />
+	<?php echo JHtml::_('form.token'); ?>
+</form>
+
+<div id="podcast_suite_link">
+	<p><a href="http://podcastsuite.com">podcastsuite.com</p>
+</div>
--- administrator/components/com_podcast/views/mediaitem/view.html.php
+++ administrator/components/com_podcast/views/mediaitem/view.html.php
@@ -5,9 +5,29 @@ jimport( 'joomla.application.component.view');
 
 class PodcastViewMediaitem extends JView
 {
+	protected $form;
+	protected $item;
+
 	public function display($tpl = null)
 	{
+		$this->item = $this->get('Item');
+		$this->form = $this->get('Form');
+
+		$this->addToolbar();
 
 		parent::display($tpl);
+	}
+
+	public function addToolbar()
+	{
+		if ($this->item->media_id) {
+			JToolBarHelper::title(JText::_('COM_PODCAST_MEDIA_ITEM_EDIT'));
+		} else {
+			JToolBarHelper::title(JText::_('COM_PODCAST_MEDIA_ITEM_ADD'));
+		}
+
+		JToolBarHelper::apply('mediaitem.apply');
+		JToolBarHelper::save('mediaitem.save');
+		JToolBarHelper::cancel('mediaitem.cancel');
 	}
 }
