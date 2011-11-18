1ad0f9ab5d2f247a4ead939bbcc40a299493c68f
Joe LeBlanc <contact@jlleblanc.com>
2011-11-18 16:56:16 -0600

    Added a 'feed' field type.

---
+++ administrator/components/com_podcast/models/fields/feed.php
@@ -0,0 +1,31 @@
+<?php
+defined( '_JEXEC' ) or die;
+
+jimport('joomla.html.html');
+jimport('joomla.form.formfield');
+jimport('joomla.form.helper');
+JFormHelper::loadFieldClass('list');
+
+class JFormFieldFeed extends JFormFieldList
+{
+	protected function _getFeeds()
+	{
+		$db = JFactory::getDBO();
+		$query = $db->getQuery(true);
+
+		$query->select('feed_id AS `value`, feed_title AS `text`')
+			->from('#__podcast_feeds');
+
+		$db->setQuery($query);
+		return $db->loadObjectList();
+	}
+
+	public function getOptions()
+	{
+		$feeds = $this->_getFeeds();
+
+		array_unshift($feeds, JHtml::_('select.option', '', '- Select Feed -'));
+
+		return $feeds;
+	}
+}
