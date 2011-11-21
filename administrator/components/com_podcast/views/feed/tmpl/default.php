30e1933e82a4766984f7cab3fc37f183d6e4174a
Joe LeBlanc <contact@jlleblanc.com>
2011-11-21 13:28:10 -0600

    Finished the essential parts of the single feed view.

--- administrator/components/com_podcast/views/feed/tmpl/default.php
+++ administrator/components/com_podcast/views/feed/tmpl/default.php
@@ -3,16 +3,51 @@
 	method="post" name="adminForm" class="form-validate">
 	<div class="width-60 fltlft">
 		<fieldset class="adminform">
-			<legend>Feed</legend>
+			<legend><?php echo JText::_('COM_PODCAST_FEED'); ?></legend>
 			<ul class="adminformlist">
-				<?php foreach ($this->form->getFieldset() as $field): ?>
+				<?php foreach ($this->form->getFieldset('top-col-1') as $field): ?>
 					<li><?php echo $field->label; ?>
 					<?php echo $field->input; ?></li>
 				<?php endforeach ?>
 			</ul>
 
+			<ul class="adminformlist">
+				<?php foreach ($this->form->getFieldset('top-col-2') as $field): ?>
+					<li><?php echo $field->label; ?>
+					<?php echo $field->input; ?></li>
+				<?php endforeach ?>
+			</ul>
+		</fieldset>
+
+		<fieldset class="adminform">
+
+			<ul class="adminformlist">
+				<?php foreach ($this->form->getFieldset('bottom') as $field): ?>
+					<li><?php echo $field->label; ?>
+					<?php echo $field->input; ?></li>
+				<?php endforeach ?>
+			</ul>
+
+
 		</fieldset>
 	</div>
+
+	<div class="width-40 fltrt">
+		<fieldset class="adminform">
+			<legend><?php echo JText::_('COM_PODCAST_FEED_EXTENDED'); ?></legend>
+
+			<ul class="adminformlist">
+				<?php foreach ($this->form->getFieldset('right') as $field): ?>
+					<li><?php echo $field->label; ?>
+					<?php echo $field->input; ?></li>
+				<?php endforeach ?>
+			</ul>
+
+
+		</fieldset>
+
+	</div>
+
 
 	<input type="hidden" name="task" value="" />
 	<?php echo JHtml::_('form.token'); ?>
--- administrator/components/com_podcast/views/feed/view.html.php
+++ administrator/components/com_podcast/views/feed/view.html.php
@@ -13,6 +13,21 @@ class PodcastViewFeed extends JView
 		$this->form = $this->get('Form');
 		$this->item = $this->get('Item');
 
+		$this->addToolbar();
+
 		parent::display($tpl);
+	}
+
+	public function addToolbar()
+	{
+		if ($this->item->feed_id) {
+			JToolBarHelper::title(JText::_('COM_PODCAST_FEED_ITEM_EDIT'));
+		} else {
+			JToolBarHelper::title(JText::_('COM_PODCAST_FEED_ITEM_ADD'));
+		}
+
+		JToolBarHelper::apply('feed.apply');
+		JToolBarHelper::save('feed.save');
+		JToolBarHelper::cancel('feed.cancel');
 	}
 }
