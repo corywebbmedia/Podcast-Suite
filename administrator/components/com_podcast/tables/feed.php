77b38f2fa8bc3b2aa3339f624321859c2bc62525
Joe LeBlanc <contact@jlleblanc.com>
2011-11-28 14:01:04 -0600

    Reworked the alias fields in the feed and item records.

--- administrator/components/com_podcast/tables/feed.php
+++ administrator/components/com_podcast/tables/feed.php
@@ -7,4 +7,19 @@ class PodcastTableFeed extends JTable
 	{
 		parent::__construct('#__podcast_feeds', 'feed_id', $db);
 	}
+
+	public function check()
+	{
+		if (trim($this->alias) == '') {
+			$this->alias = $this->feed_title;
+		}
+
+		$this->alias = JApplication::stringURLSafe($this->alias);
+
+		if (trim(str_replace('-', '', $this->alias)) == '') {
+			$this->alias = JFactory::getDate()->format('Y-m-d-H-i-s');
+		}
+
+		return true;
+	}
 }
