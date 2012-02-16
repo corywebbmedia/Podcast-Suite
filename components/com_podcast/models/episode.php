e981091a8de59a129bd30b24900071faa301fff5
Joe LeBlanc <contact@jlleblanc.com>
2012-02-16 10:44:17 -0600

    Fixed the frontend models to reflect the new database structure.

--- components/com_podcast/models/episode.php
+++ components/com_podcast/models/episode.php
@@ -5,29 +5,30 @@ jimport('joomla.application.component.model');
 
 class PodcastModelEpisode extends JModel
 {
-    
+
     public function getItem()
     {
         $episode_id = JRequest::getInt('episode_id', 0);
 
         $db = JFactory::getDBO();
         $query = $db->getQuery(true);
-        $query->select('tbl.*, 
-            a.asset_enclosure_url AS item_enclosure_url, 
-            a.asset_enclosure_length AS item_enclosure_length, 
+        $query->select('tbl.*,
+            a.asset_enclosure_url AS item_enclosure_url,
+            a.asset_enclosure_length AS item_enclosure_length,
             a.asset_duration AS item_duration,
             a.asset_enclosure_type AS item_enclosure_type,
             a.asset_closed_caption AS item_closed_caption')
                 ->from('#__podcast_episodes AS tbl')
-                ->join('LEFT', '#__podcast_assets AS a ON tbl.episode_id = a.episode_id')
-                ->where('a.default = 1')
+                ->join('LEFT', '#__podcast_assets_map AS m USING(episode_id)')
+				->join('LEFT', '#__podcast_assets AS a USING(asset_id)')
+                ->where('m.default = 1')
                 ->where('tbl.episode_id = '.$episode_id)
                 ->limit(1);
         $db->setQuery($query);
-        
+
         return $db->loadObject();
     }
-    
+
     public function getAssets($pk = null)
     {
         $episode_id = JRequest::getInt('episode_id', 0);
@@ -38,7 +39,7 @@ class PodcastModelEpisode extends JModel
                 ->from('#__podcast_assets AS tbl')
                 ->where('tbl.episode_id = '.$episode_id);
         $db->setQuery($query);
-        
+
         return $db->loadObjectList();
     }
 }
--- components/com_podcast/models/feed.php
+++ components/com_podcast/models/feed.php
@@ -42,14 +42,15 @@ class PodcastModelFeed extends JModelList
 		$feed_id = $this->getFeed()->feed_id;
 
 		$query->select('tbl.*,
-            a.asset_enclosure_url AS item_enclosure_url, 
-            a.asset_enclosure_length AS item_enclosure_length, 
+            a.asset_enclosure_url AS item_enclosure_url,
+            a.asset_enclosure_length AS item_enclosure_length,
             a.asset_duration AS item_duration,
             a.asset_enclosure_type AS item_enclosure_type,
             a.asset_closed_caption AS item_closed_caption')
             ->from('#__podcast_episodes AS tbl')
-            ->join('LEFT', '#__podcast_assets AS a ON tbl.episode_id = a.episode_id')
-            ->where('a.default = 1')
+			->join('LEFT', '#__podcast_assets_map AS m USING(episode_id)')
+            ->join('LEFT', '#__podcast_assets AS a USING(asset_id)')
+            ->where('m.default = 1')
 			->where("tbl.feed_id = '{$feed_id}'")
 			->where("tbl.published = 1");
 
