4416fb5e8d80f167594ba5b246abe8df92063421
Joe LeBlanc <contact@jlleblanc.com>
2012-03-01 18:47:43 -0600

    Deferring to JModelAdmin::getItem, this one doesn't work for new records and is otherwise superfluous

--- administrator/components/com_podcast/models/episode.php
+++ administrator/components/com_podcast/models/episode.php
@@ -17,28 +17,6 @@ class PodcastModelEpisode extends JModelAdmin
 		return $form;
 	}
 
-    public function getItem($pk = null)
-    {
-		$pk = (!empty($pk)) ? $pk : (int) $this->getState($this->getName() . '.id');
-
-		$db = JFactory::getDBO();
-		$query = $db->getQuery(true);
-		$query->select('tbl.*, a.asset_enclosure_url, a.asset_enclosure_length')
-				->select('a.asset_duration, a.asset_enclosure_type')
-				->select('a.asset_closed_caption')
-				->select('GROUP_CONCAT(m.podcast_asset_id separator ",") AS item_assets')
-				->from('#__podcast_episodes AS tbl')
-				->join('LEFT', '#__podcast_assets_map AS m ON tbl.episode_id = m.episode_id')
-				->join('LEFT', '#__podcast_assets AS a ON m.podcast_asset_id = a.podcast_asset_id')
-				->order('m.default')
-				->where('tbl.episode_id = '.$pk)
-				->group('tbl.episode_id')
-				->limit(1);
-        $db->setQuery($query);
-
-        return $db->loadObject();
-    }
-
     public function getAssets()
     {
 		$item = $this->getItem();
