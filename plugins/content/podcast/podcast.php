bade0ce3389306c0238c20f64cee2fa72ffe5fce
Joe LeBlanc <contact@jlleblanc.com>
2012-03-02 15:23:14 -0600

    Added some initial function signatures for the content plugin

--- plugins/content/podcast/podcast.php
+++ plugins/content/podcast/podcast.php
@@ -1,3 +1,15 @@
 <?php
 defined( '_JEXEC' ) or die;
 
+class plgContentPodcast extends JPlugin
+{
+	function onContentPrepare($context, &$row, &$params, $page = 0)
+	{
+
+	}
+
+	private function _getEpisodeMedia()
+	{
+
+	}
+}
