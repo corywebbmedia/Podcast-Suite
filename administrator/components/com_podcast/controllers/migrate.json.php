e66075af85b07f2b73a116283c43f3188bec01d7
Joe LeBlanc <contact@jlleblanc.com>
2012-03-12 17:29:10 -0500

    Bootstrapping the model with the path to the old Joomla install.

--- administrator/components/com_podcast/controllers/migrate.json.php
+++ administrator/components/com_podcast/controllers/migrate.json.php
@@ -9,12 +9,17 @@ class PodcastControllerMigrate extends JController
 	{
 		JRequest::checkToken() or jexit( JText::_('JINVALID_TOKEN') );
 
+		$model = $this->_getModelWithPath();
+		$model->import_feeds();
+
 		echo json_encode(array('message' => 'feeds imported', 'status' => 'success'));
 	}
 
 	public function import_podcast_episodes()
 	{
 		JRequest::checkToken() or jexit( JText::_('JINVALID_TOKEN') );
+
+		$model = $this->_getModelWithPath();
 
 		echo json_encode(array('message' => 'episodes imported', 'status' => 'success'));
 	}
@@ -23,12 +28,16 @@ class PodcastControllerMigrate extends JController
 	{
 		JRequest::checkToken() or jexit( JText::_('JINVALID_TOKEN') );
 
+		$model = $this->_getModelWithPath();
+
 		echo json_encode(array('message' => 'content descriptions imported', 'status' => 'success'));
 	}
 
 	public function import_files()
 	{
 		JRequest::checkToken() or jexit( JText::_('JINVALID_TOKEN') );
+
+		$model = $this->_getModelWithPath();
 
 		echo json_encode(array('message' => 'files imported', 'status' => 'success'));
 	}
@@ -37,6 +46,18 @@ class PodcastControllerMigrate extends JController
 	{
 		JRequest::checkToken() or jexit( JText::_('JINVALID_TOKEN') );
 
+		$model = $this->_getModelWithPath();
+
 		echo json_encode(array('message' => 'plugin tags translated', 'status' => 'success'));
+	}
+
+	protected function _getModelWithPath()
+	{
+		$joomla_path = JRequest::getVar('joomla_path', '');
+
+		$model = $this->getModel('Migrate');
+		$model->path = $joomla_path;
+
+		return $model;
 	}
 }
