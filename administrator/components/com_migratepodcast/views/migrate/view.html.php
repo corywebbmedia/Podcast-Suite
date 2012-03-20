26bf275573492488ca2e6fd7c41864a5e81982c5
Joe LeBlanc <contact@jlleblanc.com>
2012-03-20 14:05:28 -0500

    Fixed class name for Migrate view.

--- administrator/components/com_migratepodcast/views/migrate/view.html.php
+++ administrator/components/com_migratepodcast/views/migrate/view.html.php
@@ -3,7 +3,7 @@ defined( '_JEXEC' ) or die;
 
 jimport( 'joomla.application.component.view');
 
-class PodcastViewMigrate extends JView
+class MigratepodcastViewMigrate extends JView
 {
 	public function display($tpl = null)
 	{
